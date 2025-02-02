4781f067b27565fabfd41cb12d2153abf5f9cc61
Su-Qingmu <143875632+Su-Qingmu@users.noreply.github.com>
2025-02-02 14:06:20 +0800

    时钟

---
+++ js/config.js
@@ -0,0 +1,15 @@
+var config={
+
+    'language_type':2,
+    'font_color':'#7F7F7F',
+    'pointer_color':'#7F7F7F',
+	'background_color':'#7F7F7F',
+
+    /*'sound':0,
+    'sound_name':'cc.mp3',*/
+
+    'background_style':2,
+    'background_picture':'bg.jpg',
+    'background_video':'3.mp4',
+    'background_color':'#000000',
+}
